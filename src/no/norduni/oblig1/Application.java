98143446c8051631bae87605b3ef2a2387e2761b
eline <eline@DESKTOP-SJEHCTJ>
2016-10-18 21:59:01 +0200

    Endret README og flyttet reisende til egen fil

--- src/no/norduni/oblig1/Application.java
+++ src/no/norduni/oblig1/Application.java
@@ -11,67 +11,6 @@ package no.norduni.oblig1;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Reisende {
-	// Private variabler
-	private String navn;
-	private String kjønn;
-	private int alder;
-	private int passNo;
-	private int gruppeKode;
-
-	// Constructor metode
-	public Reisende(String navn, String kjønn, int alder, int passNo, int gruppeKode) {
-		this.navn = navn;
-		this.kjønn = kjønn;
-		this.alder = alder;
-		this.passNo = passNo;
-		this.gruppeKode = gruppeKode;
-	}
-
-	public String getNavn() {
-		return navn;
-	}
-
-	public void setNavn(String navn) {
-		this.navn = navn;
-	}
-
-	public String getKjønn() {
-		return kjønn;
-	}
-
-	public void setKjønn(String kjønn) {
-		if (kjønn == "M" || kjønn == "K") {
-			this.kjønn = kjønn;
-		}
-	}
-
-	public int getAlder() {
-		return alder;
-	}
-
-	public void setAlder(int alder) {
-		this.alder = alder;
-	}
-
-	public int getPassNo() {
-		return passNo;
-	}
-
-	public void setPassNo(int passNo) {
-		this.passNo = passNo;
-	}
-
-	public int getGruppeKode() {
-		return gruppeKode;
-	}
-
-	public void setGruppeKode(int gruppeKode) {
-		this.gruppeKode = gruppeKode;
-	}
-
-}
-
 class Gruppe {
 
 	private int gruppeKode;
